ed23f00446d34a19974d4173f4b7e05961a7f3c3
AnneKluytmans <annekluytmans1@gmail.com>
2024-11-23 12:00:54 +0100

    Created TelevisionControlle with HTTP requests

---
+++ src/main/java/nl/novi/TechItEasy/controllers/TelevisionController.java
@@ -0,0 +1,34 @@
+package nl.novi.TechItEasy.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/televisions")
+public class TelevisionController {
+
+    @GetMapping
+    public ResponseEntity<String> getAllTelevisions() {
+        return ResponseEntity.ok("All televisions");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getTelevisionById(@PathVariable("id") int id) {
+        return ResponseEntity.ok("Television with id: " + id);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addTelevision(@RequestBody String television) {
+        return ResponseEntity.ok("New television: " + television);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateTelevisionById(@PathVariable int id, @RequestBody String television) {
+        return ResponseEntity.ok("Updated television (id: " + id + ") : " + television);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTelevisionById(@PathVariable int id) {
+        return ResponseEntity.ok("Deleted television with id: " + id);
+    }
+}
